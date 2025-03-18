4ca57b5daaa147e35597c04a041983d722c6a10a
jordon0802 <jordon.chang.jj@gmail.com>
2025-03-18 22:17:04 +0800

    Edit command usage to reflect updates

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -22,18 +22,17 @@ public class AddCommand extends Command {
 
     public static final String COMMAND_WORD = "add";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book.\n"
             + "Parameters: "
             + PREFIX_NAME + "NAME "
-            + PREFIX_EMAIL + "EMAIL "
             + PREFIX_TELEGRAM + "TELEGRAM HANDLE "
-            + "[" + PREFIX_MOD + "TUT-MOD GROUP]...\n"
+            + PREFIX_EMAIL + "EMAIL "
+            + "[" + PREFIX_MOD + "MOD-TUT GROUP]...\n"
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
-            + PREFIX_PHONE + "98765432 "
-            + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_TELEGRAM + "@johndoe123 "
-            + PREFIX_MOD + "CS2103T-T21"
+            + PREFIX_EMAIL + "johnd@example.com "
+            + PREFIX_MOD + "CS2103T-T21 "
             + PREFIX_MOD + "CS2109S-T05";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
