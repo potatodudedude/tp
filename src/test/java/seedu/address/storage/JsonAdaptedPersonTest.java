2eb73d23d7fb3fe171648fa3f5fb176a548da52b
KimHan01 <teokimhan10@hotmail.com>
2025-03-19 00:56:30 +0800

    Bring storage tests back

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -1,114 +1,95 @@
-//package seedu.address.storage;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static seedu.address.storage.JsonAdaptedPerson.MISSING_FIELD_MESSAGE_FORMAT;
-//import static seedu.address.testutil.Assert.assertThrows;
-//import static seedu.address.testutil.TypicalPersons.BENSON;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.address.commons.exceptions.IllegalValueException;
-//import seedu.address.model.person.Address;
-//import seedu.address.model.person.Email;
-//import seedu.address.model.person.Name;
-//import seedu.address.model.person.Phone;
-//
-//public class JsonAdaptedPersonTest {
-//    private static final String INVALID_NAME = "R@chel";
-//    private static final String INVALID_PHONE = "+651234";
-//    private static final String INVALID_ADDRESS = " ";
-//    private static final String INVALID_EMAIL = "example.com";
-//    private static final String INVALID_TELEGRAMHANDLE = "telegram";
-//    private static final String INVALID_TAG = "#friend";
-//    private static final String INVALID_MOD = "CS2103T T21";
-//
-//    private static final String VALID_NAME = BENSON.getName().toString();
-//    private static final String VALID_EMAIL = BENSON.getEmail().toString();
-//    private static final String VALID_TELEGRAMHANDLE = BENSON.getTelegramHandle().toString();
-//    /*
-//    private static final List<JsonAdaptedTag> VALID_TAGS = BENSON.getModTutGroups().stream()
-//            .map(JsonAdaptedTag::new)
-//            .collect(Collectors.toList());
-//     */
-//    /*
-//    @Test
-//    public void toModelType_validPersonDetails_returnsPerson() throws Exception {
-//        JsonAdaptedPerson person = new JsonAdaptedPerson(BENSON);
-//        assertEquals(BENSON, person.toModelType());
-//    }
-//
-//    @Test
-//    public void toModelType_invalidName_throwsIllegalValueException() {
-//        JsonAdaptedPerson person =
-//                new JsonAdaptedPerson(INVALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
-//        String expectedMessage = Name.MESSAGE_CONSTRAINTS;
-//        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-//    }
-//
-//    @Test
-//    public void toModelType_nullName_throwsIllegalValueException() {
-//        JsonAdaptedPerson person = new JsonAdaptedPerson(null, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
-//        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
-//        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-//    }
-//
-//    @Test
-//    public void toModelType_invalidPhone_throwsIllegalValueException() {
-//        JsonAdaptedPerson person =
-//                new JsonAdaptedPerson(VALID_NAME, INVALID_PHONE, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
-//        String expectedMessage = Phone.MESSAGE_CONSTRAINTS;
-//        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-//    }
-//
-//    @Test
-//    public void toModelType_nullPhone_throwsIllegalValueException() {
-//        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, null, VALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
-//        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Phone.class.getSimpleName());
-//        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-//    }
-//
-//    @Test
-//    public void toModelType_invalidEmail_throwsIllegalValueException() {
-//        JsonAdaptedPerson person =
-//                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, INVALID_EMAIL, VALID_ADDRESS, VALID_TAGS);
-//        String expectedMessage = Email.MESSAGE_CONSTRAINTS;
-//        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-//    }
-//
-//    @Test
-//    public void toModelType_nullEmail_throwsIllegalValueException() {
-//        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, null, VALID_ADDRESS, VALID_TAGS);
-//        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName());
-//        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-//    }
-//
-//    @Test
-//    public void toModelType_invalidAddress_throwsIllegalValueException() {
-//        JsonAdaptedPerson person =
-//                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, INVALID_ADDRESS, VALID_TAGS);
-//        String expectedMessage = Address.MESSAGE_CONSTRAINTS;
-//        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-//    }
-//
-//    @Test
-//    public void toModelType_nullAddress_throwsIllegalValueException() {
-//        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, null, VALID_TAGS);
-//        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName());
-//        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
-//    }
-//
-//    @Test
-//    public void toModelType_invalidTags_throwsIllegalValueException() {
-//        List<JsonAdaptedTag> invalidTags = new ArrayList<>(VALID_TAGS);
-//        invalidTags.add(new JsonAdaptedTag(INVALID_TAG));
-//        JsonAdaptedPerson person =
-//                new JsonAdaptedPerson(VALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, invalidTags);
-//        assertThrows(IllegalValueException.class, person::toModelType);
-//    }
-//    **/
-//
-//}
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.storage.JsonAdaptedPerson.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.TelegramHandle;
+
+public class JsonAdaptedPersonTest {
+    private static final String INVALID_NAME = "R@chel";
+    private static final String INVALID_EMAIL = "example.com";
+    private static final String INVALID_TELEGRAMHANDLE = "telegram";
+    private static final String INVALID_MOD = "CS2103T T21";
+
+    private static final String VALID_NAME = BENSON.getName().toString();
+    private static final String VALID_EMAIL = BENSON.getEmail().toString();
+    private static final String VALID_TELEGRAMHANDLE = BENSON.getTelegramHandle().toString();
+    private static final List<JsonAdaptedModTutGroup> VALID_MODTUTGROUP = BENSON.getModTutGroups().stream()
+            .map(JsonAdaptedModTutGroup::new)
+            .collect(Collectors.toList());
+
+    @Test
+    public void toModelType_validPersonDetails_returnsPerson() throws Exception {
+        JsonAdaptedPerson person = new JsonAdaptedPerson(BENSON);
+        assertEquals(BENSON, person.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidName_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(INVALID_NAME, VALID_TELEGRAMHANDLE, VALID_EMAIL, VALID_MODTUTGROUP);
+        String expectedMessage = Name.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullName_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(null, VALID_TELEGRAMHANDLE, VALID_EMAIL, VALID_MODTUTGROUP);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidTelegramHandle_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, INVALID_TELEGRAMHANDLE, VALID_EMAIL, VALID_MODTUTGROUP);
+        String expectedMessage = TelegramHandle.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullTelegramHandle_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, null, VALID_EMAIL, VALID_MODTUTGROUP);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, TelegramHandle.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidEmail_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_TELEGRAMHANDLE, INVALID_EMAIL, VALID_MODTUTGROUP);
+        String expectedMessage = Email.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullEmail_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_TELEGRAMHANDLE, null, VALID_MODTUTGROUP);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Email.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidModTutGroup_throwsIllegalValueException() {
+        List<JsonAdaptedModTutGroup> invalidModTutGroup = new ArrayList<JsonAdaptedModTutGroup>(VALID_MODTUTGROUP);
+        invalidModTutGroup.add(new JsonAdaptedModTutGroup(INVALID_MOD));
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_NAME, VALID_TELEGRAMHANDLE, VALID_EMAIL, invalidModTutGroup);
+        assertThrows(IllegalValueException.class, person::toModelType);
+    }
+
+}
