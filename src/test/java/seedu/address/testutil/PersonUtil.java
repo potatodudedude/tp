c75ab6da2a1c53d89689b899f63cd9a680932e7f
KimHan01 <teokimhan10@hotmail.com>
2025-03-18 15:05:39 +0800

    Update testUtil

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -1,45 +1,50 @@
-//package seedu.address.testutil;
-//
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-//import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-//
-//import java.util.Set;
-//
-//import seedu.address.logic.commands.AddCommand;
-//import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-//import seedu.address.model.person.Person;
-//import seedu.address.model.tag.Tag;
-//
-///**
-// * A utility class for Person.
-// */
-//public class PersonUtil {
-//
-//    /**
-//     * Returns an add command string for adding the {@code person}.
-//     */
-//    public static String getAddCommand(Person person) {
-//        return AddCommand.COMMAND_WORD + " " + getPersonDetails(person);
-//    }
-//
-//    /**
-//     * Returns the part of command string for the given {@code person}'s details.
-//     */
-//    public static String getPersonDetails(Person person) {
-//        StringBuilder sb = new StringBuilder();
-//        sb.append(PREFIX_NAME + person.getName().fullName + " ");
+package seedu.address.testutil;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MOD;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TELEGRAM;
+
+import java.util.Set;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
+
+/**
+ * A utility class for Person.
+ */
+public class PersonUtil {
+
+    /**
+     * Returns an add command string for adding the {@code person}.
+     */
+    public static String getAddCommand(Person person) {
+        return AddCommand.COMMAND_WORD + " " + getPersonDetails(person);
+    }
+
+    /**
+     * Returns the part of command string for the given {@code person}'s details.
+     */
+    public static String getPersonDetails(Person person) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_NAME + person.getName().fullName + " ");
 //        sb.append(PREFIX_PHONE + person.getPhone().value + " ");
-//        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
+        sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
+        sb.append(PREFIX_TELEGRAM + person.getTelegramHandle().value + " ");
+        sb.append(PREFIX_MOD);
+        person.getModTutGroups().forEach(m -> sb.append(m.value + " "));
 //        sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
 //        person.getTags().stream().forEach(
 //            s -> sb.append(PREFIX_TAG + s.tagName + " ")
 //        );
-//        return sb.toString();
-//    }
-//
+        return sb.toString();
+    }
+
 //    /**
 //     * Returns the part of command string for the given {@code EditPersonDescriptor}'s details.
 //     */
@@ -59,4 +64,4 @@
 //        }
 //        return sb.toString();
 //    }
-//}
+}
--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -1,28 +1,28 @@
-//package seedu.address.testutil;
-//
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
-//import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import seedu.address.model.AddressBook;
-//import seedu.address.model.person.Person;
-//
-///**
-// * A utility class containing a list of {@code Person} objects to be used in tests.
-// */
-//public class TypicalPersons {
-//
+package seedu.address.testutil;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.person.Person;
+
+/**
+ * A utility class containing a list of {@code Person} objects to be used in tests.
+ */
+public class TypicalPersons {
+
 //    public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
 //            .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
 //            .withPhone("94351253")
@@ -41,36 +41,59 @@
 //            .withEmail("lydia@example.com").withAddress("little tokyo").build();
 //    public static final Person GEORGE = new PersonBuilder().withName("George Best").withPhone("9482442")
 //            .withEmail("anna@example.com").withAddress("4th street").build();
-//
+
+    public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
+            .withEmail("alice@example.com").withTele("@alice")
+            .withModTut("CS1234-A10").build();
+    public static final Person BENSON = new PersonBuilder().withName("Benson Meier")
+            .withEmail("johnd@example.com").withTele("@benson")
+            .withModTut("CS1234-A10").build();
+    public static final Person CARL = new PersonBuilder().withName("Carl Kurz")
+            .withEmail("heinz@example.com").withTele("@carl_kurz")
+            .withModTut("CS1234-A10").build();
+    public static final Person DANIEL = new PersonBuilder().withName("Daniel Meier")
+            .withEmail("cornelia@example.com").withTele("@daniel")
+            .withModTut("CS1234-A10").build();
+    public static final Person ELLE = new PersonBuilder().withName("Elle Meyer")
+            .withEmail("werner@example.com").withTele("@elle")
+            .withModTut("ES2666-Z01").build();
+    public static final Person FIONA = new PersonBuilder().withName("Fiona Kunz")
+            .withEmail("lydia@example.com").withTele("@fiona")
+            .withModTut("ES2666-Z01").build();
+    public static final Person GEORGE = new PersonBuilder().withName("George Best")
+            .withEmail("anna@example.com").withTele("@george")
+            .withModTut("ES2666-Z01").build();
+
 //    // Manually added
 //    public static final Person HOON = new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
 //            .withEmail("stefan@example.com").withAddress("little india").build();
 //    public static final Person IDA = new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
 //            .withEmail("hans@example.com").withAddress("chicago ave").build();
-//
+
 //    // Manually added - Person's details found in {@code CommandTestUtil}
 //    public static final Person AMY = new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
 //            .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY).withTags(VALID_TAG_FRIEND).build();
 //    public static final Person BOB = new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
 //            .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB).withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND)
 //            .build();
-//
-//    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
-//
-//    private TypicalPersons() {} // prevents instantiation
-//
-//    /**
-//     * Returns an {@code AddressBook} with all the typical persons.
-//     */
-//    public static AddressBook getTypicalAddressBook() {
-//        AddressBook ab = new AddressBook();
-//        for (Person person : getTypicalPersons()) {
-//            ab.addPerson(person);
-//        }
-//        return ab;
-//    }
-//
-//    public static List<Person> getTypicalPersons() {
-//        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
-//    }
-//}
+
+    public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
+
+    private TypicalPersons() {
+    } // prevents instantiation
+
+    /**
+     * Returns an {@code AddressBook} with all the typical persons.
+     */
+    public static AddressBook getTypicalAddressBook() {
+        AddressBook ab = new AddressBook();
+        for (Person person : getTypicalPersons()) {
+            ab.addPerson(person);
+        }
+        return ab;
+    }
+
+    public static List<Person> getTypicalPersons() {
+        return new ArrayList<>(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE));
+    }
+}
