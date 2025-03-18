b9b6744c5c95192ea1d852db926e1cadaae356b7
KimHan01 <teokimhan10@hotmail.com>
2025-03-18 23:09:19 +0800

    Update Message Constraints

--- src/main/java/seedu/address/model/person/TelegramHandle.java
+++ src/main/java/seedu/address/model/person/TelegramHandle.java
@@ -11,7 +11,8 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class TelegramHandle {
 
     public static final String MESSAGE_CONSTRAINTS = "First character in telegram handle should be @, "
-            + "followed by at least five characters";
+            + "followed by at least five alphanumeric characters. "
+            + "Underscore is allowed, but not at the start or the end";
     private static final String VALIDATION_REGEX = "^@[a-zA-Z0-9_]{5,}";
 
     public final String value;
