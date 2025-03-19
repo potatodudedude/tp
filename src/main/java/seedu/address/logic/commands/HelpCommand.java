b6149cd78d4fe7faed0bffddd17d4c133738f700
jordon0802 <jordon.chang.jj@gmail.com>
2025-03-19 21:30:38 +0800

    Fix help command to directly reference message usage

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -19,61 +19,15 @@ public class HelpCommand extends Command {
 
     public static final String SHOWING_HELP_MESSAGE = HelpCommand.showHelpMessage();
 
-    private static String[] listJavaFiles(File directory) {
-        if (!directory.isDirectory() || !directory.exists()) {
-            return null;
-        } else {
-            String[] directoryList = directory.list((dir, name) -> name.endsWith(".java"));
-
-            if (directoryList == null) {
-                return null;
-            }
-
-            return Arrays.stream(directoryList).map((name) -> {
-                String[] nameArray = name.split("/");
-                int length = nameArray.length;
-                return nameArray[length - 1];
-            }).toArray(String[]::new);
-        }
-    }
-
     private static String showHelpMessage() {
-        StringBuilder message = new StringBuilder();
-
-        File directory = new File("./src/main/java/seedu/address/logic/commands/");
-        String[] fileStrings = listJavaFiles(directory);
-
-        if (fileStrings == null) {
-            return "";
-        }
-
-        for (String string : fileStrings) {
-            String fileString = "seedu.address.logic.commands." + string;
-
-            if (!fileString.endsWith("Command.java")
-                    || string.equals("Command.java")) {
-                continue;
-            }
-
-            int index = fileString.indexOf(".java");
-            fileString = fileString.substring(0, index);
-
-            try {
-                Class<?> commandClass = Class.forName(fileString);
-                String helpMessage = (String) commandClass.getDeclaredField("MESSAGE_USAGE").get(null);
-
-                if (helpMessage == null) {
-                    helpMessage = "MESSAGE_USAGE not implemented...";
-                }
-
-                message.append(helpMessage).append("\n\n");
-            } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException e) {
-                Logger logger = LogsCenter.getLogger(HelpCommand.class);
-                logger.warning(e.getMessage());
-            }
-        }
-
-        return message.toString().strip();
+        return AddCommand.MESSAGE_USAGE + "\n\n" +
+                ClearCommand.MESSAGE_USAGE + "\n\n" +
+                DeleteCommand.MESSAGE_USAGE + "\n\n" +
+                EditCommand.MESSAGE_USAGE + "\n\n" +
+                ExitCommand.MESSAGE_USAGE + "\n\n" +
+                FindCommand.MESSAGE_USAGE + "\n\n" +
+                HelpCommand.MESSAGE_USAGE + "\n\n" +
+                ListCommand.MESSAGE_USAGE;
     }
 
     @Override
