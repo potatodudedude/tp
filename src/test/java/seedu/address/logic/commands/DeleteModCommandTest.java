a4deb1703cc4d230bd265d41b325a5c032509331
e1093051 <e1093051@u.nus.edu>
2025-04-06 23:43:26 +0800

    Add test for deleteMod

--- src/test/java/seedu/address/logic/commands/DeleteModCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteModCommandTest.java
@@ -2,11 +2,9 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MODTUT_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MOD_AMY;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_MOD_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.logic.commands.CommandTestUtil.*;
 import static seedu.address.logic.commands.DeleteModCommand.MESSAGE_DELETE_MOD_SUCCESS;
+import static seedu.address.logic.commands.DeleteModCommand.MESSAGE_MOD_NOT_EXIST;
 import static seedu.address.testutil.TypicalPersons.getSortedTypicalAddressBook;
 
 import java.lang.reflect.Field;
@@ -14,7 +12,6 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -25,12 +22,6 @@ import seedu.address.model.person.Module;
 
 public class DeleteModCommandTest {
     private Model model = new ModelManager(getSortedTypicalAddressBook(), new UserPrefs());
-
-    public void setUp() throws Exception {
-        // Set up a clean AddressBook and reset moduleMap before each test
-        model = new ModelManager(new AddressBook(), new UserPrefs());
-        resetModuleMap();
-    }
 
     private void resetModuleMap() throws Exception {
         Field mapField = ModTutGroup.class.getDeclaredField("moduleMap");
@@ -45,11 +36,9 @@ public class DeleteModCommandTest {
     }
 
 
-
-
     @Test
     public void execute_validMod_success() throws Exception {
-        Module moduleToDelete = new Module("CS1234");
+        Module moduleToDelete = new Module(VALID_MOD_AMY);
         DeleteModCommand deleteModCommand = new DeleteModCommand(moduleToDelete);
 
         String expectedMessage = String.format(MESSAGE_DELETE_MOD_SUCCESS, moduleToDelete.toString());
@@ -61,6 +50,12 @@ public class DeleteModCommandTest {
         assertCommandSuccess(deleteModCommand, model, expectedMessage, expectedModel);
     }
 
+    @Test
+    public void execute_nonExistModDelete_throwsCommandException() {
+        Module moduleToDelete = new Module("CS0000");
+        DeleteModCommand deleteModCommand = new DeleteModCommand(moduleToDelete);
+        assertCommandFailure(deleteModCommand, model, MESSAGE_MOD_NOT_EXIST + moduleToDelete.toString());
+    }
 
 
     @Test
