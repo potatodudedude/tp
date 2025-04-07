9c0edb33a19064134d37119e9cfe6ecb71dfba72
e1093051 <e1093051@u.nus.edu>
2025-04-07 17:27:52 +0800

    Add parseMod

--- src/main/java/seedu/address/logic/parser/DeleteModCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteModCommandParser.java
@@ -1,6 +1,5 @@
 package seedu.address.logic.parser;
 
-import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.logic.commands.DeleteModCommand;
@@ -22,16 +21,8 @@ public class DeleteModCommandParser implements Parser<DeleteModCommand> {
      */
     @Override
     public DeleteModCommand parse(String args) throws ParseException {
-        requireNonNull(args);
-        String keyword = args.trim();
-
-        if (keyword.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteModCommand.MESSAGE_USAGE));
-        }
-
         try {
-            Module module = new Module(keyword.toUpperCase());
+            Module module = ParserUtil.parseModule(args);
             return new DeleteModCommand(module);
         } catch (Exception e) {
             throw new ParseException(
