4b36cc4564173b6230a0e697ffb970800adbb4f5
KimHan01 <teokimhan10@hotmail.com>
2025-04-08 02:22:52 +0800

    fix style

--- src/main/java/seedu/address/logic/parser/exceptions/EmptyIndexException.java
+++ src/main/java/seedu/address/logic/parser/exceptions/EmptyIndexException.java
@@ -1,5 +1,8 @@
 package seedu.address.logic.parser.exceptions;
 
+/**
+ * Signals that the index provided is empty.
+ */
 public class EmptyIndexException extends RuntimeException {
     public EmptyIndexException(String message) {
         super(message);
